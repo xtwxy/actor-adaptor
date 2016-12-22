7c822d37def387a4c1b8d1ddf236d1d7691d5997
Wangxy <xtwxy@hotmail.com>
2016-12-22 18:14:49 +0800

    add restful ws.

---
+++ webapp/src/main/java/com/wincom/actor/adaptor/webapi/ActorResource.java
@@ -0,0 +1,8 @@
+package com.wincom.actor.adaptor.webapi;
+
+import javax.ws.rs.Path;
+
+@Path("actors")
+public class ActorResource {
+
+}
---
+++ webapp/src/main/java/com/wincom/actor/adaptor/webapi/ComponentsResource.java
@@ -0,0 +1,34 @@
+package com.wincom.actor.adaptor.webapi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.wincom.actor.adaptor.messages.ActorType;
+import com.wincom.actor.adaptor.messages.Components;
+import com.wincom.actor.adaptor.messages.PortNameList;
+
+@Path("components")
+public class ComponentsResource {
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getIt() {
+    	Components c = new Components();
+    	for(int i = 0; i < 5; ++i) {
+    		ActorType e = new ActorType();
+    		e.setInput("input");
+    		e.setName("TestComponent-" + i);
+    		e.setTypeid("com.wincom.actor.authorization_1.0" + i);
+    		
+    		PortNameList pnl = new PortNameList();
+    		pnl.getPort().add("output-1");
+    		pnl.getPort().add("output-2");
+			e.setOutput(pnl );
+    		
+			c.getActorTypes().getActorType().add(e);
+    	}
+        return Response.ok().entity(c).build();
+    }
+}
